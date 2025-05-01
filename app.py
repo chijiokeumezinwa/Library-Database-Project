ca8429e9f10da02a7d24c865829301c2c57e09d8
chijiokeumezinwa <chijiokeumezinwa@gmail.com>
2025-05-01 09:58:01 -0400

    Beginning integration of database with front end

--- app.py
+++ app.py
@@ -1,7 +1,45 @@
+import os
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from dotenv import load_dotenv
 
 app = Flask(__name__)
+
+
+load_dotenv()
+
+#credentials
+db_user = os.environ['DB_USER']
+db_password = os.environ['DB_PASSWORD']
+db_name = os.environ['DB_NAME']
+
 app.config.from_object('config')
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "mysql://{db_user}:{db_password}@localhost/{db_name}".format(
+    db_user=db_user, db_password=db_password, db_name=db_name)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+# Define a Model for your table
+class user(db.Model):
+    __tablename__ = 'users'  # Specify your actual table name
+
+    # Define your columns here
+    user_id  = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(100), nullable=False)
+    password = db.Column(db.String(255), nullable=False)
+    email = db.Column(db.String(255), nullable=False)
+    is_verified = db.Column(db.Boolean, default=False)
+    is_admin = db.Column(db.Boolean, default=False)
+    
+                 
+    def __repr__(self):
+        return f'<User {self.user_id}, {self.username}>'
+
+@app.errorhandler(401)
+def FUN_401(error):
+    return render_template("page_401.html"), 401
+
 
 @app.route('/')
 def home():
@@ -10,6 +48,8 @@ def home():
 @app.route('/about')
 def about():
     return render_template('about.html')
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ config.py
@@ -0,0 +1,5 @@
+#Flask Config File
+
+SECRET_KEY = "pgnhfdac"
+UPLOAD_FOLDER = "image_pool"
+MAX_CONTENT_LENGTH = 16 * 1024 * 1024
