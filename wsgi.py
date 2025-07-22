7145e5d1f7101707e53b77c429a465d1d7d7cf01
Ubuntu <ubuntu@ip-172-31-33-0.us-east-2.compute.internal>
2025-07-22 17:39:44 +0000

    new changes

--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,14 @@
-# wsgi.py
+import sys
+import os
+
+# Add your project directory to the Python path
+project_home = u'/home/ubuntu/projects/Library-Database-Project'
+if project_home not in sys.path:
+    sys.path.insert(0, project_home)
+
+# Import the create_app function from app.py
 from app import create_app
 
-app = create_app()
-
-if __name__ == '__main__':
-    app.run()
+# Create the Flask application instance and name it 'application'
+# Gunicorn looks for a callable named 'application' by default
+application = create_app()
